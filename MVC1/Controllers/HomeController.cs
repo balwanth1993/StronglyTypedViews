7d9ad4fdf8528aa167b0a5f09b1636fcc99885b6
balwanth1993 <balwanth.r93@gmail.com>
2016-12-08 18:27:37 -0600

    Add project files.

---
+++ MVC1/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC1.Models;
+
+namespace MVC1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<EmployeeClass> employeeClass = new List<EmployeeClass>()
+            {
+                new EmployeeClass {Name="Balwanth" , Id=1, Salary=5000 },
+                new EmployeeClass {Name="Raamji" , Id=2, Salary=6000 },
+                new EmployeeClass {Name="Venu" , Id=3, Salary=5000 }
+
+            };
+            ViewBag.List = employeeClass;
+            return View();
+        }
+        public ActionResult Detail()
+        {
+            List<EmployeeClass> employeeClass = new List<EmployeeClass>()
+            {
+                new EmployeeClass {Name="Balwanth" , Id=1, Salary=5000 },
+                new EmployeeClass {Name="Raamji" , Id=2, Salary=6000 },
+                new EmployeeClass {Name="Venu" , Id=3, Salary=5000 }
+
+            };
+            return View(employeeClass);
+            
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVC1/Models/EmployeeClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC1.Models
+{
+    public class EmployeeClass
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int Salary { get; set; }
+    }
+}
